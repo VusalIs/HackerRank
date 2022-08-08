9096304803d159ceb90b8852adf4a65656fa73b3
VusalIs <41757170+VusalIs@users.noreply.github.com>
2022-08-08 21:07:35 +0400

    Create java_method_override.java

---
+++ Java/java_method_override.java
@@ -0,0 +1,35 @@
+/*
+
+Complete the code in your editor by writing an
+overridden getNumberOfTeamMembers method that 
+prints the same statement as the superclass' 
+getNumberOfTeamMembers method, except that 
+it replaces n with 11 (the number of players 
+on a Soccer team).
+
+*/
+
+import java.util.*;
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+
+}
